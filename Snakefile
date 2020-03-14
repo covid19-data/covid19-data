073bc1e439c6cda3ccf36d7d6d654e8e4ba622a3
YY Ahn <yongyeol@gmail.com>
2020-03-13 21:21:47 -0400

    try using owid

--- Snakefile
+++ Snakefile
@@ -16,16 +16,16 @@ WB_META = j(WB_DATA_DIR, 'Metadata_Country_API_SP.POP.TOTL_DS2_en_csv_v2_821007.
 WB_RAW = j(WB_DATA_DIR, 'wb_raw.csv')
 
 # Population data cleaning and matching with JHU data
-POP_ADDITION = j(WB_DATA_DIR, 'pop_addition.csv')
-POP_CONVERSION = j(WB_DATA_DIR, 'pop_conversion.csv')
-POP_CLEANED_CSV = j(WB_DATA_DIR, 'pop.csv')
+# POP_ADDITION = j(WB_DATA_DIR, 'pop_addition.csv')
+# POP_CONVERSION = j(WB_DATA_DIR, 'pop_conversion.csv')
+# POP_CLEANED_CSV = j(WB_DATA_DIR, 'pop.csv')
 
 # Final json file for the visualization
-CNTRY_STAT_JSON = 'cntry_stat.json'
+CNTRY_STAT_JSON = 'output/cntry_stat.json'
+CNTRY_STAT_JSON_FROM_OWID = 'output/cntry_stat_owid.json'
 
-include: "snakemake_tableau.smk"
-include: "snakemake_owid.smk"
-#include: "data_sources/our_world_in_data/Snakefile"
+include: "sm_owid.smk"
+include: "sm_tableau.smk"
 #include: "data_sources/worldbank_population_data/Snakefile"
 
 rule all:
@@ -35,6 +35,11 @@ rule extract_coordinates:
     input: TABLEAU_TS
     output: COORDINATES
     script: "scripts/extract_coordinates.py"
+
+rule prepare_viz_data:
+    input: OWID_TS, WB_RAW
+    output: CNTRY_STAT_JSON_FROM_OWID
+    script: "scripts/prepare_viz_data_owid.py"
 
 # rule combine_ts:
     # input: CONFIRMED_TS, DEATHS_TS, RECOVERED_TS
---
+++ scripts/prepare_viz_data_owid.py
@@ -0,0 +1,52 @@
+"""Prepare the CFR data visualization data from OWID (WHO)."""
+import json
+
+import pandas as pd
+
+# Load the OWID data
+df = (
+    pd.read_csv(snakemake.input[0], index_col=0, parse_dates=True)
+    .rename(columns={"location": "country"})
+    .fillna(0, downcast="infer")
+)
+
+# full date range
+idx = pd.date_range(df.index.min(), df.index.max())
+all_dates = idx.strftime("%Y-%m-%d")
+
+# Load the worldbank data.
+country_dict = (
+    pd.read_csv(
+        snakemake.input[1], usecols=["country", "population", "region", "income"]
+    )
+    .replace(
+        {
+            "Brunei Darussalam": "Brunei",
+            "Congo, Dem. Rep.": "Democratic Republic of Congo",
+            "Egypt, Arab Rep.": "Egypt",
+            "Iran, Islamic Rep.": "Iran",
+            "Korea, Rep.": "South Korea",
+        }
+    )
+    .set_index("country")
+    .to_dict()
+)
+
+data = []
+for group in df.groupby(["country"]):
+    cntry_df = group[1][["total_cases", "total_deaths"]].reindex(idx, fill_value=0)
+    try:
+        country_data = {
+            "country": group[0],
+            "population": country_dict["population"][group[0]],
+            "region": country_dict["region"][group[0]],
+            "income": country_dict["income"][group[0]],
+            "confirmed": list(zip(all_dates, cntry_df["total_cases"])),
+            "deaths": list(zip(all_dates, cntry_df["total_deaths"])),
+        }
+        data.append(country_data)
+    except KeyError:
+        print("key error!", group[0])
+        continue
+
+open(snakemake.output[0], "w").write(json.dumps(data))
