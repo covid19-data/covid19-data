ef9d374a2a50311c7499bdf9d3cfd258e8b73b01
YY Ahn <yongyeol@gmail.com>
2020-03-15 14:49:40 -0400

    download script update

--- data_sources/our_world_in_data/download.sh
+++ data_sources/our_world_in_data/download.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
-wget -q cowid.netlify.com/data/full_data.csv
-DIFF=`diff full_data.csv owid_ts.csv`
-if [ "$DIFF" != "" ] 
-then
-    mv full_data.csv owid_ts.csv
-    echo "data updated."
+CURR_DATA="owid_ts.csv"
+NEW_DATA="full_data.csv"
+
+wget -q -O $NEW_DATA cowid.netlify.com/data/full_data.csv
+
+if [ ! -f "$CURR_DATA" ]; then
+    if [ -f "$NEW_DATA" ]; then
+	mv $NEW_DATA $CURR_DATA 
+	echo "data downloaded."
+    else
+	echo "data download error."
+    fi 
 else
-    rm full_data.csv
-    echo "data not yet updated."
+    DIFF=`diff $NEW_DATA $CURR_DATA`
+    if [ "$DIFF" != "" ] 
+    then
+	mv $NEW_DATA $CURR_DATA
+	echo "data updated."
+    else
+	rm $NEW_DATA
+	echo "data not yet updated."
+    fi
 fi
+
--- data_sources/tableau/download.sh
+++ data_sources/tableau/download.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
-wget -q -O temp.csv "https://docs.google.com/spreadsheets/d/1avGWWl1J19O_Zm0NGTGy2E-fOG05i4ljRfjl87P7FiA/gviz/tq?tqx=out:csv&sheet=COVID-19"
-DIFF=`diff temp.csv tableau_ts.csv`
-if [ "$DIFF" != "" ] 
-then
-    mv temp.csv tableau_ts.csv
-    echo "data updated."
+CURR_DATA="tableau_ts.csv"
+NEW_DATA="temp.csv"
+
+wget -q -O $NEW_DATA "https://docs.google.com/spreadsheets/d/1avGWWl1J19O_Zm0NGTGy2E-fOG05i4ljRfjl87P7FiA/gviz/tq?tqx=out:csv&sheet=COVID-19"
+
+if [ ! -f "$CURR_DATA" ]; then
+    if [ -f "$NEW_DATA" ]; then
+	mv $NEW_DATA $CURR_DATA 
+	echo "data downloaded."
+    else
+	echo "data download error."
+    fi 
 else
-    rm temp.csv
-    echo "data not yet updated."
+    DIFF=`diff $NEW_DATA $CURR_DATA`
+    if [ "$DIFF" != "" ] 
+    then
+	mv $NEW_DATA $CURR_DATA
+	echo "data updated."
+    else
+	rm $NEW_DATA
+	echo "data not yet updated."
+    fi
 fi
