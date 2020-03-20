1bdaa094f41c8ca82c3050d31d670591288f42e5
benjyz <ben@enet.io>
2020-03-20 13:51:57 +0700

    csv only

--- data_sources/worldometer/worldometer_scraper.py
+++ data_sources/worldometer/worldometer_scraper.py
@@ -16,6 +16,8 @@ def get_page():
     daily_table = soup.find(id='main_table_countries_today')
     rows = daily_table.find_all('tr')
     country_data = {}
+    country_data_csv = []
+    
     for row in rows[:-1]:
         try:
             cols = row.find_all('td')
@@ -23,12 +25,28 @@ def get_page():
             country = cols[0].text.strip()
             total_cases = cols[1].text.strip()
             total_cases = total_cases.replace(",","")
-            print (country,total_cases)
+            new_cases = cols[2].text.strip()
+            new_cases = new_cases.replace(",","")
+            new_cases = new_cases.replace("+","")
+            td = cols[3].text.strip().replace(",","")
+            nd = cols[4].text.strip().replace(",","")
+            nd = nd.replace("+","")
+
             country_data[country] = total_cases
+            s = "\t".join([country, total_cases, new_cases, td, nd])
+            country_data_csv.append(s)
         except:
             continue
 
-    with open('worldometer_country.json','w') as f:
-        f.write(json.dumps(country_data))
+    #with open('worldometer_country.json','w') as f:
+    #    f.write(json.dumps(country_data))
+
+    with open('worldometer_country.csv','w') as f:
+        h = "\t".join(["country", "total cases", "new cases", "total deaths", "new deaths"])
+        f.write(h +'\n')
+        for r in country_data_csv:
+            f.write(r + '\n')
+
 
 get_page()
+
