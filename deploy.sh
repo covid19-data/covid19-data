5b82c16de91647508b714ea900bddba98cb094af
hongtaoh <fhaohongtao@163.com>
2020-10-27 16:56:24 -0400

    updating data on Tue Oct 27 16:56:24 EDT 2020

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 mv dataNineNewRegionAdded.json output/cntry_stat_owid.json
 python scripts/nyt_state_data.py
+python scripts/prepare_state_level_viz_data.py 
 git add .
 msg="updating data on $(date)" 
 git commit -m "$msg"
