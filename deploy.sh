72626ba5e5d668647e6c9b1ae79bc3187a2ad4ae
hongtaoh <fhaohongtao@163.com>
2020-10-29 11:21:30 -0400

    updating data on Thu Oct 29 11:21:30 EDT 2020

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
-if test -f "$dataNineNewRegionAdded.json"; then
-	mv dataNineNewRegionAdded.json output/cntry_stat_owid.json
-fi
+if [ -f "dataNineNewRegionAdded.json" ]; then mv dataNineNewRegionAdded.json output/cntry_stat_owid.json; fi
 python scripts/nyt_state_data.py
 python scripts/prepare_state_level_viz_data.py 
 git add .
 msg="updating data on $(date)" 
 git commit -m "$msg"
 git push
+
+
