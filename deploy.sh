dadc7792ca982a8647fb47bb0e265399c3168d6d
hongtaoh <fhaohongtao@163.com>
2020-10-29 11:22:28 -0400

    updating data on Thu Oct 29 11:22:28 EDT 2020

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
-if [ -f "dataNineNewRegionAdded.json" ]; then mv dataNineNewRegionAdded.json output/cntry_stat_owid.json; fi
+if [ -f "dataNineNewRegionAdded.json" ]; 
+	then mv dataNineNewRegionAdded.json output/cntry_stat_owid.json; 
+fi
 python scripts/nyt_state_data.py
 python scripts/prepare_state_level_viz_data.py 
 git add .
