88748cbfe02e2dffd69922f22f02c46562d68ad0
hongtaoh <fhaohongtao@163.com>
2021-07-09 09:38:41 -0400

    update workflow

--- deprecated/deploy.sh
+++ deprecated/deploy.sh
@@ -5,9 +5,9 @@ julia scripts/prepare_owid_viz_data.jl
 
 julia scripts/prepare_state_level_viz_data.jl
 
-# git add .
-# msg="updating data on $(date)" 
-# git commit -m "$msg"
-# git push
+git add .
+msg="updating data on $(date)" 
+git commit -m "$msg"
+git push
 
 
